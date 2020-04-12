544abed86c56bb5580262dc6ae6f6d7f7b67f4c3
Cioccio <pablo.cioccio@thomsonreuters.com>
2020-04-11 22:26:41 -0300

    Agregando funciones

---
+++ funciones.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    const mensaje = "Hola!";
+    secondFunction(mensaje);
+}
+
+function secondFunction(parametro) {
+    alert(parametro);
+}
+
+function funcionSimple(primerNumero, segundoNumero) {
+    const resultado = primerNumero / segundoNumero;
+    alert(resultado);
+}
